e6ec7d57626dfbe13acff36d4083a52b1c4f8465
jcduff00 <johncduffman@gmail.com>
2023-05-08 14:57:51 -0400

    created band tab

--- BandTab.js
+++ BandTab.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
+import metalData from './metal.json';
 
 const BandTab = () => {
+  const BandItem = ({ band }) => {
+    return (
+      <View>
+        <Text>Band Name: {band.band_name}</Text>
+        <Text>Country of Origin: {band.origin}</Text>
+        <Text>Number of Fans: {band.fans}</Text>
+        <Text>Year Formed: {band.formed}</Text>
+      </View>
+    );
+  };
+
   return (
     <View>
-      <Text>Welcome to the band tab!</Text>
+      <FlatList
+        data={metalData}
+        keyExtractor={(item) => item.band_name}
+        renderItem={({ item }) => <BandItem band={item} />}
+      />
     </View>
   );
 };
